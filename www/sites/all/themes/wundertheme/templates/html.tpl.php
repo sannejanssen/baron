493eaa2bbd8bd684f8b8ed80f5cde54a53cae629
Sanne Janssen <sanne.janssen@wunderkraut.com>
2013-08-12 20:56:55 +0200

    update theme

--- www/sites/all/themes/wundertheme/templates/html.tpl.php
+++ www/sites/all/themes/wundertheme/templates/html.tpl.php
@@ -64,7 +64,7 @@
     <?php print $head; ?>
     <meta name="MobileOptimized" content="width">
     <meta name="HandheldFriendly" content="true">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
     <meta http-equiv="cleartype" content="on">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <?php print $styles; ?>
